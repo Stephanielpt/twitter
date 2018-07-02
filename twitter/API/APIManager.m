da88b9ab30ee7221f3c4e3c8cc4952c47db85586
Stephanie Lampotang <slampotang@fb.com>
2018-07-02 13:23:05 -0700

    first commit

---
+++ twitter/API/APIManager.m
@@ -0,0 +1,77 @@
+//
+//  APIManager.m
+//  twitter
+//
+//  Created by emersonmalca on 5/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import "APIManager.h"
+
+static NSString * const baseURLString = @"https://api.twitter.com";
+static NSString * const consumerKey = @"60e1d3f9485ce67b2f7c2f12b424a95dU809QqUfHK8skbvy9vz6ECJ60"; // Enter your consumer key here
+static NSString * const consumerSecret = @"axTkGtAuyDjoVnE4DxLVgyKdMnPuvauqt7daJqu6YYU7hjPJLh"; // Enter your consumer secret here
+
+@interface APIManager()
+
+@end
+
+@implementation APIManager
+
++ (instancetype)shared {
+    static APIManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (instancetype)init {
+    
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSString *key = consumerKey;
+    NSString *secret = consumerSecret;
+    // Check for launch arguments override
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"]) {
+        key = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"];
+    }
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"]) {
+        secret = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"];
+    }
+    
+    self = [super initWithBaseURL:baseURL consumerKey:key consumerSecret:secret];
+    if (self) {
+        
+    }
+    return self;
+}
+
+//+ (void)logout {
+//    // 1. Clear current user
+//    User.current = nil;
+//    
+//    // TODO: 2. Deauthorize OAuth tokens
+//    
+//    // 3. Post logout notification
+//    [[NSNotificationCenter defaultCenter] postNotificationName:@"didLogout" object:nil];
+//}
+
+- (void)getHomeTimelineWithCompletion:(void(^)(NSArray *tweets, NSError *error))completion {
+    
+    [self GET:@"1.1/statuses/home_timeline.json"
+   parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSArray *  _Nullable tweetDictionaries) {
+       
+       // Cache it
+       NSData *data = [NSKeyedArchiver archivedDataWithRootObject:tweetDictionaries];
+       [[NSUserDefaults standardUserDefaults] setValue:data forKey:@"hometimeline_tweets"];
+
+       if (completion) { completion(tweetDictionaries, nil); }
+       
+   } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+       
+       if (completion) { completion(nil, error); }
+   }];
+}
+
+@end
