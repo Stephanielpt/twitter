7d974a2b36e7235cb12f353898bc0f72965a56f5
Stephanie Lampotang <slampotang@fb.com>
2018-07-05 15:24:02 -0700

    the details view is perfect but not connected to the timeline so when you go back the timeline hasn't been refreshed

--- twitter/View Controllers/DetailTweetView.m
+++ twitter/View Controllers/DetailTweetView.m
@@ -9,6 +9,7 @@
 #import "DetailTweetView.h"
 #import "UIImageView+AFNetworking.h"
 #import <QuartzCore/QuartzCore.h>
+#import "APIManager.h"
 
 @interface DetailTweetView ()
 @property (weak, nonatomic) IBOutlet UIImageView *profilePic;
@@ -20,6 +21,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *retweetLabel;
 @property (weak, nonatomic) IBOutlet UILabel *likeCountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *likeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *retweetButton;
+@property (weak, nonatomic) IBOutlet UIButton *favoriteButton;
 
 @end
 
@@ -49,6 +52,84 @@
     }
     else {
         self.likeLabel.text = @"Likes";
+    }
+    self.retweetButton.selected = self.tweet.retweeted;
+    self.favoriteButton.selected = self.tweet.favorited;
+}
+
+- (IBAction)onTapRetweet:(id)sender {
+    // TODO: Update the local tweet model
+    self.tweet.retweeted = !self.tweet.retweeted;
+    if(self.tweet.retweeted)
+    {
+        self.tweet.retweetCount += 1;
+    }
+    else {
+        self.tweet.retweetCount -= 1;
+    }
+    self.retweetCountLabel.text = [NSString stringWithFormat:@"%d", self.tweet.retweetCount];
+    // TODO: Update cell UI
+    self.retweetButton.selected = self.tweet.retweeted;
+    //[self.delegate didRetweet:self.tweet];
+    // TODO: Send a POST request to the POST retweeted/create endpoint
+    if(self.tweet.retweeted)
+    {
+        [[APIManager shared] retweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        [[APIManager shared] unretweet:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error retweeting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully retweeted the following Tweet: %@", tweet.text);
+            }
+        }];
+    }}
+
+- (IBAction)onTapFavorite:(id)sender {
+    // TODO: Update the local tweet model
+    self.tweet.favorited = !self.tweet.favorited;
+    if(self.tweet.favorited)
+    {
+        self.tweet.favoriteCount += 1;
+    }
+    else {
+        self.tweet.favoriteCount -= 1;
+    }
+    self.likeCountLabel.text = [NSString stringWithFormat:@"%d", self.tweet.favoriteCount];
+    // TODO: Update cell UI
+    self.favoriteButton.selected = self.tweet.favorited;
+    // refreshCell()
+    //[self.delegate didLike:self.tweet];
+    // TODO: Send a POST request to the POST favorites/create endpoint
+    if(self.tweet.favorited)
+    {
+        [[APIManager shared] favorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
+    }
+    else {
+        [[APIManager shared] unfavorite:self.tweet completion:^(Tweet *tweet, NSError *error) {
+            if(error){
+                NSLog(@"Error favoriting tweet: %@", error.localizedDescription);
+            }
+            else{
+                NSLog(@"Successfully favorited the following Tweet: %@", tweet.text);
+            }
+        }];
     }
 }
 
